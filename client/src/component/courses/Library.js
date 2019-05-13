afcc11ab39eb413d632e47b4aaf6cf1611a21187
Raghav <raghavsahuja@gmail.com>
2019-05-13 17:26:39 +0530

    colors set

--- client/src/component/courses/Library.js
+++ client/src/component/courses/Library.js
@@ -1,122 +1,151 @@
 import React from 'react'
-import { MDBIcon, MDBCard, MDBCardFooter, MDBCardBody, MDBCardText, MDBCardHeader, MDBBtn, MDBRow} from "mdbreact"
+import { MDBIcon, MDBCard, MDBCardFooter, MDBCardBody, MDBCardText, MDBCardHeader, MDBBtn, MDBRow } from "mdbreact"
 const Library = (props) => {
+    const cardStyle = {
+        width: "22rem",
+        margin: "1rem"
+    }
     return (
-        <div className="container">
-            <div className="input-group m-form form-sm form-1 pl-0 mt-5 mb-5">
+        <div className="container-fluid">
+            <div className="input-group m-form form-sm form-1 px-5 mt-5 mb-2">
                 <div className="input-group-prepend">
-                <span className="input-group-text cyan lighten-2" id="basic-text1">
-                    <MDBIcon icon="search" className="text-grey" />
-                </span>
+                    <span className="input-group-text cyan lighten-2" id="basic-text1">
+                        <MDBIcon icon="search" className="text-grey" />
+                    </span>
                 </div>
-                <input className="form-control my-0 py-1" type="text" placeholder="Search" />
+                <input className="form-control" type="text" placeholder="Search" />
             </div>
-
-            <MDBRow center>
-                <MDBCard style={{ width: "22rem", margin: ".25rem" }}>
-                    <MDBCardHeader color="primary-color" tag="h4">
-                        Basic javascript
-                    </MDBCardHeader>
-                    <MDBCardBody>
-                        <MDBCardText>
-                        Refactor an existing authentication project by using cookies and JSON Web Token to increase security. Cookies are a way for a browser to store information while tokens are a stand-in ...
+            <div style={{'max-width':'1300px', margin:'auto'}}>
+                <MDBRow center>
+                    <MDBCard style={cardStyle}>
+                        <MDBCardHeader
+                            style={{ background: "var(--brownCard)", color: "white", padding: '15px' }}
+                            tag="h6">
+                            Basic javascript
+                         </MDBCardHeader>
+                        <MDBCardBody>
+                            <MDBCardText>
+                                Refactor an existing authentication project by using cookies and JSON Web Token to increase security. Cookies are a way for a browser to store information while tokens are a stand-in ...
                         </MDBCardText>
-                    </MDBCardBody>
-                    <footer className="m-4" small muted>
-                        <div className="flex-row">
-                            <MDBCardText className="float-left" small muted>3 hours</MDBCardText>
-                            <MDBBtn className="float-right" color="dark" onClick={()=>{
-                                props.history.push('/course/1')
-                            }}>Start</MDBBtn>
-                        </div>
+                        </MDBCardBody>
+                        <footer className="m-4" small muted>
+                            <div className="d-flex align-items-center justify-content-between">
+                                <MDBCardText small muted>3 hours</MDBCardText>
+                                <MDBBtn
+                                    style={{background: "var(--brownCard)", border:"0px", color: "white"}}
+                                    onClick={() => {
+                                        props.history.push('/course/1')
+                                    }}>Start</MDBBtn>
+                            </div>
                         </footer>
-                </MDBCard>
+                    </MDBCard>
 
-                <MDBCard style={{ width: "22rem", margin: ".25rem" }}>
-                    <MDBCardHeader color="primary-color" tag="h4">
-                        ES6
+                    <MDBCard className="bg-blueCard" style={cardStyle}>
+                        <MDBCardHeader
+                            style={{ background: "var(--blueCard)", color: "white", padding: '15px' }}
+                            tag="h6">
+                            ES6
                     </MDBCardHeader>
-                    <MDBCardBody>
-                        <MDBCardText>
-                        ECMAScript 6 is also known as ES6 and ECMAScript 2015. Some people call it JavaScript 6. This chapter will introduce some of the new features in ES6.
+                        <MDBCardBody>
+                            <MDBCardText style={{ "color": "#687173" }}>
+                                ECMAScript 6 is also known as ES6 and ECMAScript 2015. Some people call it JavaScript 6. This chapter will introduce some of the new features in ES6.
                         </MDBCardText>
-                    </MDBCardBody>
-                    <footer className="m-4">
-                        <div className="flex-row">
-                            <MDBCardText className="float-left" small muted>100 min left</MDBCardText>
-                            <MDBBtn className="float-right" color="dark">Resume</MDBBtn>
-                        </div>
+                        </MDBCardBody>
+                        <footer className="m-4">
+                        <div className="d-flex align-items-center justify-content-between">
+                                <MDBCardText
+                                    small>100 min left</MDBCardText>
+                                <MDBBtn 
+                                 style={{background: "var(--blueCard)", border:"0px", color: "white"}}
+                                color="dark">
+                                Resume
+                                </MDBBtn>
+                            </div>
                         </footer>
-                </MDBCard>
+                    </MDBCard>
 
-                <MDBCard style={{ width: "22rem", margin: ".25rem" }}>
-                    <MDBCardHeader color="primary-color" tag="h4">
-                        Advance React and Redux
+                    <MDBCard style={cardStyle}>
+                        <MDBCardHeader
+                            style={{ background: "var(--pinkCard)", color: "white", padding: '15px' }}
+                            tag="h6">
+                            Advance React and Redux
                     </MDBCardHeader>
-                    <MDBCardBody>
-                        <MDBCardText>
-                           Refactor an existing authentication project by using cookies and JSON Web Token to increase security. 
+                        <MDBCardBody>
+                            <MDBCardText>
+                                Refactor an existing authentication project by using cookies and JSON Web Token to increase security.
                         </MDBCardText>
-                    </MDBCardBody>
-                    <footer className="m-4">
-                        <div className="flex-row">
-                            <MDBCardText className="float-left" small muted>Completed</MDBCardText>
-                            <MDBBtn className="float-right"><i className="fas fa-check-circle fa-2x"></i></MDBBtn>
-                        </div>
+                        </MDBCardBody>
+                        <footer className="m-4">
+                        <div className="d-flex align-items-center justify-content-between">
+                                <MDBCardText small muted>Completed</MDBCardText>
+                                    <i className="far fa-check-circle fa-2x"  style={{background: "var(--white)", color: "var(--pinkCard)"}}></i>
+                            </div>
                         </footer>
-                </MDBCard>
+                    </MDBCard>
 
-                <MDBCard style={{ width: "22rem", margin: ".25rem" }}>
-                    <MDBCardHeader color="primary-color" tag="h4">
-                        Node js
+                    <MDBCard style={cardStyle}>
+                        <MDBCardHeader
+                            style={{ background: "var(--orangeCard)", color: "white", padding: '15px' }}
+                            tag="h6">
+                            Node js
                     </MDBCardHeader>
-                    <MDBCardBody>
-                        <MDBCardText>
-                        Refactor an existing authentication project by using cookies and JSON Web Token to increase security. Cookies are a way for a browser to store information while tokens are a stand-in ...
+                        <MDBCardBody>
+                            <MDBCardText>
+                                Refactor an existing authentication project by using cookies and JSON Web Token to increase security. Cookies are a way for a browser to store information while tokens are a stand-in ...
                         </MDBCardText>
-                    </MDBCardBody>
-                    <footer className="m-4">
-                        <div className="flex-row">
-                            <MDBCardText className="float-left" small muted>3 hours</MDBCardText>
-                            <MDBBtn className="float-right" color="dark">Start</MDBBtn>
-                        </div>
+                        </MDBCardBody>
+                        <footer className="m-4">
+                        <div className="d-flex align-items-center justify-content-between">
+                                <MDBCardText  small muted>3 hours</MDBCardText>
+                                <MDBBtn 
+                                 style={{background: "var(--orangeCard)", border:"0px", color: "white"}}
+                                color="dark">
+                                Start</MDBBtn>
+                            </div>
                         </footer>
-                </MDBCard>
-                <MDBCard style={{ width: "22rem", margin: ".25rem" }}>
-                    <MDBCardHeader color="primary-color" tag="h4">
-                        Mongo DB
+                    </MDBCard>
+                    <MDBCard style={cardStyle}>
+                        <MDBCardHeader
+                            style={{ background: "var(--purpleCard)", color: "white", padding: '15px' }}
+                            color="primary-color" tag="h6">
+                            Mongo DB
                     </MDBCardHeader>
-                    <MDBCardBody>
-                        <MDBCardText>
-                        Refactor an existing authentication project by using cookies and JSON Web Token to increase security. Cookies are a way for a browser to store information while tokens are a stand-in ...
+                        <MDBCardBody>
+                            <MDBCardText>
+                                Refactor an existing authentication project by using cookies and JSON Web Token to increase security. Cookies are a way for a browser to store information while tokens are a stand-in ...
                         </MDBCardText>
-                    </MDBCardBody>
-                    <footer className="m-4">
-                        <div className="flex-row">
-                            <MDBCardText className="float-left" small muted>3 hours</MDBCardText>
-                            <MDBBtn className="float-right" color="dark">Start</MDBBtn>
-                        </div>
-                    </footer>
-                </MDBCard>
-                <MDBCard style={{ width: "22rem", margin: ".25rem" }}>
-                    <MDBCardHeader color="primary-color" tag="h4">
-                        Three js
+                        </MDBCardBody>
+                        <footer className="m-4">
+                        <div className="d-flex align-items-center justify-content-between">
+                                <MDBCardText small muted>3 hours</MDBCardText>
+                                <MDBBtn 
+                                 style={{background: "var(--purpleCard)", border:"0px", color: "white"}}
+                                 color="dark">Start</MDBBtn>
+                            </div>
+                        </footer>
+                    </MDBCard>
+                    <MDBCard style={cardStyle}>
+                        <MDBCardHeader
+                            style={{ background: "var(--redCard)", color: "white", padding: '15px' }}
+                            color="primary-color" tag="h6">
+                            Three js
                     </MDBCardHeader>
-                    <MDBCardBody>
-                        <MDBCardText>
-                        Refactor an existing authentication project by using cookies and JSON Web Token to increase security. Cookies are a way for a browser to store information while tokens are a stand-in ...
+                        <MDBCardBody>
+                            <MDBCardText>
+                                Refactor an existing authentication project by using cookies and JSON Web Token to increase security. Cookies are a way for a browser to store information while tokens are a stand-in ...
                         </MDBCardText>
-                    </MDBCardBody>
-                    <footer className="m-4">
-                        <div className="flex-row">
-                            <MDBCardText className="float-left" small muted>3 hours</MDBCardText>
-                            <MDBBtn className="float-right" color="dark">Start</MDBBtn>
-                        </div>
-                    </footer>
-                </MDBCard>
-
-            </MDBRow>
-
+                        </MDBCardBody>
+                        <footer className="m-4">
+                        <div className="d-flex align-items-center justify-content-between">
+                                <MDBCardText small muted>3 hours</MDBCardText>
+                                <MDBBtn 
+                                 style={{background: "var(--redCard)", border:"0px", color: "white"}}
+                                color="dark">Start</MDBBtn>
+                            </div>
+                        </footer>
+                    </MDBCard>
+                </MDBRow>
+            </div>
         </div>
     )
 }
