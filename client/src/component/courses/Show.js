64af36f34ecbcab96bb89bd27c8fda40ed78b439
Raghav <raghavsahuja@gmail.com>
2019-05-13 22:46:51 +0530

    added custom colors for progress bar inside course show page

--- client/src/component/courses/Show.js
+++ client/src/component/courses/Show.js
@@ -45,7 +45,7 @@ const CourseShow = (props) =>{
                 <div className="d-flex flex-row flex-fill">
                     <MDBBtn className="mr-2"><h6> 20 steps <i className="fas fa-angle-double-down"></i></h6></MDBBtn>
                     <div style={{width:'10rem'}} className="my-auto">
-                        <MDBProgress value={100} >100</MDBProgress>
+                        <MDBProgress value={100} color="greenHighlight">100</MDBProgress>
                     </div>
                 </div>
                 </MDBMedia>
@@ -62,7 +62,7 @@ const CourseShow = (props) =>{
                 <div className="d-flex flex-row flex-fill">
                     <MDBBtn className="mr-2"><h6> 12 steps <i className="fas fa-angle-double-down"></i></h6></MDBBtn>
                     <div style={{width:'10rem'}} className="my-auto">
-                        <MDBProgress color='danger' value={50} >50</MDBProgress>
+                        <MDBProgress color='greenHighlight' value={50} >50</MDBProgress>
                     </div>
                 </div>
                 
